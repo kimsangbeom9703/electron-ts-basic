e63ea5ac2be06212549c09c14f922507aef50e21
kimsangbeom9703 <kp41589272@gmail.com>
2024-04-05 11:28:27 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,46 @@
 import {app, BrowserWindow} from 'electron';
-import path from "path";
+import * as path from "path";
 
 let mainWindow: Electron.BrowserWindow | null;
 
-app.on('ready', () => {
+
+const createWindow = async () => {
+    // browser window를 생성합니다.
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        // fullscreen: true,
+        // frame: false,
+        // kiosk: true,
+        // alwaysOnTop: true,
+        // show: false,
         webPreferences: {
+            webviewTag: true,
             nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js') // 필요에 따라 preload 스크립트 추가
+            backgroundThrottling: false,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
         }
-    });
+    })
 
-    mainWindow.loadFile(path.join(__dirname, "../index.html"));
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
-});
+    mainWindow.loadFile(path.join(__dirname, "../views/index.html"));
+    mainWindow.webContents.openDevTools();
+    return mainWindow;
+}
+app.whenReady().then(async() => {
+    await createWindow()
+
+    // Linux와 Winodws 앱은 browser window가 열려 있지 않을 때 종료됩니다.
+    // macOS는 browser window가 열려 있지 않아도 계속 실행되기 때문에,
+    // browser window가 열려 있지 않을 때 앱을 활성화 하면 새로운 browser window를 열어줍니다.
+    app.on('activate', async () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            await createWindow()
+        }
+    })
+})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
